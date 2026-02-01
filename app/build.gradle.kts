497dd68058cc182f18ba0714c1ba497fa2ed9cdb
hoihan kim <rossgaller@DESKTOP-O51K5KS.localdomain>
2026-02-01 20:16:26 +0900

    chore: Add material-icons-extended dependency for pause/play icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.material)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
